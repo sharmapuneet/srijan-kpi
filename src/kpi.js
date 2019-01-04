ad87610948bfc21e36d4a355d390ab7d0a4f2247
Puneet Sharma <puneetsharma@Puneets-MacBook-Pro.local>
2019-01-05 00:16:07 +1000

    srijan-kpi

---
+++ src/kpi.js
@@ -0,0 +1,37 @@
+export default srijanKpi({
+  data: {
+    kpis: [
+      {
+        category: 'Behavioural',
+        kpi: {
+          'Team Work / Supporting the team in completions tasks at hand',
+          'Communication',
+          'Mentoring Peers',
+          'Investing in self-growth',
+          'Hard working/Commitment level',
+          'Are you picking up complex tickets/task/functionality?',
+          'Punctuality',
+          'Project/Business priority/Big picture?',
+          'Ability to negotiate, push-back',
+        },
+      },
+      {
+        category: 'Programming Skills',
+        kpi: {
+          'Readability or maintainability of code',
+          'Ability to write code where each unit has single responsibility and is unit testable',
+          'Are coding standards being followed?',
+          'Code reusability',
+          'Code comments and documentation',
+        },
+      },
+      {
+        category: 'Debugging Skills',
+        kpi: {
+          'Where and What to Debug',
+          'Understanding of various tools relevant to the tech stack ',
+        },
+      },
+    ],
+  },
+})
