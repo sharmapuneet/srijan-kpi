380d63b2fe3b582c239d3bc4e25e0541121ea080
Puneet Sharma <puneetsharma@Puneets-MacBook-Pro.local>
2019-04-09 21:49:41 +1000

    change in name

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export default srijanEmployees({
   return [
-    { name: 'Puneet Sharma', title : 'Technical Lead L1' },
+    { name: 'Puneet Kumar Sharma', title : 'Technical Lead L1' },
     { name: 'Abhinav Rana', title : 'Senior Drupal Developer L2' },
     { name: 'Apoorv Mathur', title : 'Frontend Technical Lead L1' },
     { name: 'Anul Khadija', title : 'Drupal Developer L2' },
